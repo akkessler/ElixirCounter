891cc17a23416816346c7f4804b7471cab394556
Andy Kessler <akkessler@users.noreply.github.com>
2017-09-15 18:16:56 -0500

    Add event handlers, set text as number, temporarily toast on click

--- app/src/main/java/io/akessler/elixircounter/CounterButton.java
+++ app/src/main/java/io/akessler/elixircounter/CounterButton.java
@@ -4,22 +4,30 @@ import android.content.Context;
 import android.support.v7.widget.AppCompatButton;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
 /**
  * Created by Andy on 9/15/2017.
  */
-public class CounterButton extends AppCompatButton implements View.OnTouchListener, View.OnClickListener {
+public class CounterButton extends AppCompatButton implements OnTouchListener, OnClickListener {
 
+    private Context context;
     private int counterValue;
 
     public CounterButton(Context context, int counterValue) {
         super(context);
+        this.context = context;
         this.counterValue = counterValue;
+        setOnTouchListener(this);
+        setOnClickListener(this);
+        setText(String.valueOf(counterValue));
     }
 
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
+        Toast.makeText(context, String.valueOf(counterValue), Toast.LENGTH_SHORT).show();
     }
 
     @Override
