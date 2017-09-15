5f9d5f90d03b59f6c1a0a6b5edc056bf93730c00
Andy Kessler <akkessler@users.noreply.github.com>
2017-09-15 18:17:32 -0500

    Add array of CounterButtons, remove PoC code

--- app/src/main/java/io/akessler/elixircounter/OverlayService.java
+++ app/src/main/java/io/akessler/elixircounter/OverlayService.java
@@ -7,21 +7,16 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
 /**
- * Created by Andy on 8/17/2017.
+ * Created by Andy on 9/14/2017.
  */
-public class OverlayService extends Service implements View.OnTouchListener, View.OnClickListener {
+public class OverlayService extends Service {
 
     WindowManager windowManager;
-    Button button;
-    float x0, y0, xOff, yOff;
-    boolean moving;
+    Button[] buttons;
 
     @Nullable
     @Override
@@ -35,75 +30,35 @@ public class OverlayService extends Service implements View.OnTouchListener, Vie
 
         windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 
-        button = new Button(this);
-        button.setOnTouchListener(this);
-        button.setOnClickListener(this);
-        button.setText("Click me!");
-
-        WindowManager.LayoutParams buttonParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                PixelFormat.TRANSLUCENT
-        );
-        buttonParams.gravity = Gravity.LEFT | Gravity.TOP;
-        buttonParams.x = 0;
-        buttonParams.y = 0;
-
-        windowManager.addView(button, buttonParams);
+        buttons = new Button[10];
+        for(int i=0; i<buttons.length; i++) {
+//            int counterValue = i != 0 ? i : -1; // FIXME There might be a cleaner way...
+            int counterValue = i;
+            buttons[i] = new CounterButton(this, counterValue);
+            WindowManager.LayoutParams buttonParams = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, // FIXME Acts up on certain API versions
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                    PixelFormat.TRANSLUCENT
+            );
+            buttonParams.gravity = Gravity.LEFT | Gravity.TOP;
+            // FIXME Change these values to be dynamic, based on dimensions of screen
+            buttonParams.x = 25;
+            buttonParams.y = i * 150;
+            windowManager.addView(buttons[i], buttonParams);
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(button != null) {
-            windowManager.removeView(button);
-            button = null;
+        for(int i=0; i<buttons.length; i++) {
+            Button b = buttons[i];
+            if(buttons[i] != null) {
+                windowManager.removeView(b);
+                b = null;
+            }
         }
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                int[] buttonLocation = new int[2];
-                button.getLocationOnScreen(buttonLocation);
-
-                x0 = buttonLocation[0];
-                y0 = buttonLocation[1];
-                button.setText(buttonLocation[0] +";"+ buttonLocation[1]);
-
-                xOff = x0 - event.getRawX();
-                yOff = y0 - event.getRawY();
-                moving = false;
-
-                break;
-            case MotionEvent.ACTION_MOVE:
-                int x = (int) (xOff + event.getRawX());
-                int y = (int) (yOff + event.getRawY());
-                if(Math.abs(x - x0) < 1 && Math.abs(y - y0) < 1 && !moving) {
-                    return false;
-                }
-
-                WindowManager.LayoutParams layoutParams =  (WindowManager.LayoutParams) button.getLayoutParams();
-                layoutParams.x = x;
-                layoutParams.y = y - button.getHeight();
-
-                windowManager.updateViewLayout(button, layoutParams);
-                moving = true;
-
-                break;
-            case MotionEvent.ACTION_UP:
-                return moving;
-            default:
-        }
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
     }
 }
