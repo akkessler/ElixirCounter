e59cb614c55cfc387915cc5356a4b2d5803a6230
Andy Kessler <akkessler@users.noreply.github.com>
2017-09-22 01:11:28 -0500

    Add permissions check to allow recording audio with mic

--- app/src/main/java/io/akessler/elixircounter/MainActivity.java
+++ app/src/main/java/io/akessler/elixircounter/MainActivity.java
@@ -1,14 +1,47 @@
 package io.akessler.elixircounter;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Used to handle permission request */
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Check if user has given permission to record audio
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            return;
+        }
+
+        startOverlayService();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startOverlayService();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    private void startOverlayService() {
         Intent overlayService = new Intent(this, OverlayService.class);
         startService(overlayService);
         finish();
